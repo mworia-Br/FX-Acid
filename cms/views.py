0a4a1a5f81ddfc1939f7d0d4dd734e8c8b9c4a89
Brian Nganga <briannganga70@gmail.com>
2023-09-27 00:34:35 +0300

    Forgot Password

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
 
 # Create your views here.
+def indexfr(request):
+    return render(request, 'login&signup.html')
+
+def resetpassword(request):
+    return render(request, 'forgotpassword.html')
