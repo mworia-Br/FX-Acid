7ae2f579c748e15132e68d073921375f3f13819a
Brian Nganga <briannganga70@gmail.com>
2023-10-04 20:18:43 +0300

    remove url acounts/

--- cms/urls.py
+++ cms/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('services/', views.ServiceView, name="services"),
     path('systems/', views.SystemView, name="systems"),
     path('contact/', views.ContactView, name="contact"),
-    path('resetpassword/', views.resetpassword, name="reset"),
-    path('accounts/', views.accounts, name="accounts")
+    path('resetpassword/', views.resetpassword, name="reset")
+    #path('accounts/', views.accounts, name="accounts")
 ]
